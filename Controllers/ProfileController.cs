e1b4e04f60af0c11a29f91e2e928a8eae7e2358d
Nikita <sorokin.nikita2003@yandex.ru>
2025-01-23 13:57:43 +0300

    added api for registration

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -5,19 +5,61 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Data;
 using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Providers.Entities;
 
 namespace agency.Controllers
 {
-    [Authorize]
+    [Route("api/[controller]")]
+    // [Authorize]
     public class ProfileController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProfileController(ApplicationDbContext context)
+        private readonly UserManager<IdentityUser> _userManager; // Добавим UserManager для регистрации
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+
+        public ProfileController(ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _context = context;
+            _userManager = userManager; // Инициализируем UserManager
+            _signInManager = signInManager;
         }
+
+        // Метод для регистрации клиента
+        [HttpPost("RegisterClient")]
+        public async Task<IActionResult> RegisterClient([FromBody] RegisterClientModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser { Email = model.Email, UserName = model.Email };
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    await _userManager.AddToRoleAsync(user, "BUYER");
+                    return Ok(model);
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+            return BadRequest("Некорректные данные");
+        }
+
+        // Модель для регистрации покупателя
+        //public class RegisterClientModel
+        //{
+        //    public string UserName { get; set; }
+        //    public string Password { get; set; }
+        //}
+
+        // Пример других методов контроллера
+
         public async Task<IActionResult> Profile()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -25,7 +67,7 @@ namespace agency.Controllers
 
             return View(user);
         }
-        //MyOrders
+
         public IActionResult MyOrders()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -48,29 +90,32 @@ namespace agency.Controllers
 
             return RedirectToAction(nameof(MyOrders));
         }
-        [Authorize(Roles = "TourOperator")]
-        public async Task<IActionResult> Stat() 
+
+        [HttpGet("Stat")]
+        // [Authorize(Roles = "TourOperator")]
+        public async Task<IActionResult> Stat()
         {
             Dictionary<string, int> countrys = new Dictionary<string, int>();
             var ot = await _context.OrderedTour.ToListAsync();
 
-            foreach(var country in ot)
+            foreach (var country in ot)
             {
                 country.Tour = await _context.Tour.FindAsync(country.TourId);
             }
 
-            foreach(var i in ot)
+            foreach (var i in ot)
             {
-                if(countrys.Keys.Contains(i.Tour.Country))
+                if (countrys.Keys.Contains(i.Tour.Country))
                 {
                     countrys[i.Tour.Country] += i.PersonsCount;
-                } else
+                }
+                else
                 {
                     countrys[i.Tour.Country] = i.PersonsCount;
                 }
             }
 
-            return View(countrys);
+            return Ok(countrys);
         }
     }
 }
